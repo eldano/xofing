b87bf78a776b80ff59484b0c199ff8d7f4902e0b
eldano.v2 <eldano.v2@81424038-bd86-11dd-baed-41e496641fac>
2009-03-01 03:45:16 +0000

    Clase para crear colisionadores genéricos

---
+++ ProyGado/src/framework/base/colisionador.py
@@ -0,0 +1,22 @@
+from framework.base.base import *
+
+class Colisionador(Component):
+    punto1 = (0,0)
+    punto2 = (0,0)
+    normalVector = (0,0)
+    
+    def __init__(self, punto1, punto2, normalVect):
+        self.punto1 = punto1
+        self.punto2 = punto2
+        self.normalVector = normalVect
+    
+    def checkCollision(self, component):
+        cx = component.x
+        cy = component.y
+        cw = component.width
+        ch = component.height
+        
+        if (cx + cw > self.punto1[0] and cx < self.punto2[0]):
+            if(cy <= self.punto1[1] and cy + ch > self.punto1[1]):
+                return True
+        return False
