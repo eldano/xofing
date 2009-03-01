c5341c69a8c85550babc4ee168b7bbead4d27856
eldano.v2 <eldano.v2@81424038-bd86-11dd-baed-41e496641fac>
2009-03-01 21:00:05 +0000

    Agregué colisionador Horizontal y Vertical

--- ProyGado/src/framework/base/colisionador.py
+++ ProyGado/src/framework/base/colisionador.py
@@ -10,6 +10,7 @@ class Colisionador(Component):
         self.punto2 = punto2
         self.normalVector = normalVect
     
+class ColisionadorHz(Colisionador):
     def checkCollision(self, component):
         cx = component.x
         cy = component.y
@@ -18,5 +19,18 @@ class Colisionador(Component):
         
         if (cx + cw > self.punto1[0] and cx < self.punto2[0]):
             if(cy <= self.punto1[1] and cy + ch > self.punto1[1]):
+                return True
+        return False
+
+class ColisionadorVt(Colisionador):
+    
+    def checkCollision(self, component):
+        cx = component.x
+        cy = component.y
+        cw = component.width
+        ch = component.height
+        
+        if(cy + ch > self.punto1[1] and cy < self.punto2[1]):
+            if(cx <= self.punto1[0] and cx + cw > self.punto1[0]):
                 return True
         return False
