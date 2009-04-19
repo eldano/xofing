fe5633b6d839162ea13251443c537a4c0c82693a
eldano.v2 <eldano.v2@81424038-bd86-11dd-baed-41e496641fac>
2009-04-19 05:30:44 +0000

    Agregué las expresiones: ConstantExpression (devuelve un valor cte)
    FloatExpression (devuelve un atributo float de un component)
    
    AddFloatExpression (suma entre floats)

--- trunk/ProyGado/src/framework/expressions/Expressions.py
+++ trunk/ProyGado/src/framework/expressions/Expressions.py
@@ -12,6 +12,14 @@ class Expression:
     def evaluate(self):
         None
 
+class ConstantExpression(Expression):
+    def __init__(self, val):
+        Expression.__init__(self)
+        self.value = val
+    
+    def evaluate(self):
+        return self.value
+
 class NumericExpression(Expression):
     def __init__(self, go, family, field):
         Expression.__init__(self)
@@ -21,7 +29,19 @@ class NumericExpression(Expression):
     
     def evaluate(self):
         component = self.go.getComponent(self.family)
-        return int(getattr(component, self.field, '0'))
+        return int(getattr(component, self.field, '0'))  
+
+class FloatExpression(Expression):
+    def __init__(self, go, family, field):
+        Expression.__init__(self)
+        self.go = go
+        self.field = field
+        self.family = family
+    
+    def evaluate(self):
+        component = self.go.getComponent(self.family)
+        retorno = float(getattr(component, self.field)) 
+        return retorno
 
 class AddExpression(Expression):
     def __init__(self, exp1, exp2):
@@ -31,6 +51,17 @@ class AddExpression(Expression):
     
     def evaluate(self):
         return int(self.subs[0].evaluate()) + int(self.subs[1].evaluate())
+
+class AddFloatExpression(Expression):
+    def __init__(self, exp1, exp2):
+        Expression.__init__(self)
+        self.subs.append(exp1)
+        self.subs.append(exp2)
+    
+    def evaluate(self):
+        expr1res = float(self.subs[0].evaluate())
+        expr2res = float(self.subs[1].evaluate())
+        return  expr1res + expr2res
 
 class MulExpression(Expression):
     def __init__(self, exp1, ex2):
@@ -42,10 +73,11 @@ class MulExpression(Expression):
         return self.subs[0].evaluate()*self.subs[1].evaluate()
 
 class DivExpression(Expression):
-    def __init__(self, exp1, ex2):
+    def __init__(self, exp1, exp2):
         Expression.__init__(self)
         self.subs.append(exp1)
         self.subs.append(exp2)
             
     def evaluate(self):
         return self.subs[0].evaluate()/self.subs[1].evaluate()
+
