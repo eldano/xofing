20ab4fb28e2e4d4279a98dca37dc3016f6ad163c
mauricio.eguia <mauricio.eguia@81424038-bd86-11dd-baed-41e496641fac>
2009-04-28 06:26:19 +0000

    ejemplo 3

--- ProyGado/src/testMauricio.py
+++ ProyGado/src/testMauricio.py
@@ -6,6 +6,9 @@ Created on 01/04/2009
 
 from framework.base.base import *
 from framework.components.GraphicComponents import SpriteComponent
+from framework.components.MoveComponent import LeftRightMoveComponent
+from framework.conditions.Conditions import *
+from framework.actions.Actions import *
 import framework.base.xmlparser as xmlparser
 
 class MyLevel(GameLevel):
@@ -13,10 +16,47 @@ class MyLevel(GameLevel):
 		GameLevel.__init__(self, loop, surface)
 	
 	def populate(self):
-		go = GameObject("zero")
-		spriteInfo = xmlparser.parseSpriteFile("../data/spaceinvaderssprite.xml").values()[0] # por ahora asumo que hay solo un grupo de sprites
+		go = GameObject("invader")
+		spriteInfo = xmlparser.parseSpriteFile("../data/invadersprite.xml").values()[0] # por ahora asumo que hay solo un grupo de sprites
 		#zeroSprite = spriteInfo[0]
 		SpriteComponent(go, 100,100, spriteInfo)
+
+		pad2MovementComponent = LeftRightMoveComponent( go, 0.5, 0.001)
+
+	        leftArrowPressKeyCondition = SpecificKeyCondition(275)
+        	leftArrowReleaseKeyCondition = KeyReleaseCondition(275)
+        
+	        rightArrowPressKeyCondition = SpecificKeyCondition(276)
+        	rightArrowReleaseKeyCondition = KeyReleaseCondition(276)
+        
+        	leftArrowPressKeyCondition.addProxy(Action(go, 1, ComponentFamily.move))
+	        leftArrowReleaseKeyCondition.addProxy(Action(go, 0, ComponentFamily.move))
+        
+	        rightArrowPressKeyCondition.addProxy(Action(go, 2, ComponentFamily.move))
+        	rightArrowReleaseKeyCondition.addProxy(Action(go, 0, ComponentFamily.move))
+
+	        leftWallGO = GameObject('leftWall')
+        	leftWallGraphicComponent = Line(leftWallGO, 0, 0, 0, 480, (0,255,255), 5)
+	        leftWallBVComponent = VerticalCollider(leftWallGO, 0, 0, 480, (1,0))
+        	leftWallCollisionCondition = CollisionCondition(leftWallGO, ballGO)
+	        leftWallCollisionCondition.addProxy(BounceAction(ballGO, leftWallGO))
+        
+        	rightWallGO = GameObject('rightWall')
+	        rightWallGraphicComponent = Line(rightWallGO, 640, 0, 640, 480, (0,255,255), 5)
+        	rightWallBVComponent = VerticalCollider(rightWallGO, 640, 0, 480, (-1,0))
+	        rightWallCollisionCondition = CollisionCondition(rightWallGO, ballGO)
+        	rightWallCollisionCondition.addProxy(BounceAction(ballGO, rightWallGO))
+
+
+	        self.gameLoop.conditions.append(leftArrowPressKeyCondition)
+        	self.gameLoop.conditions.append(leftArrowReleaseKeyCondition)
+	        self.gameLoop.conditions.append(rightArrowPressKeyCondition)
+        	self.gameLoop.conditions.append(rightArrowReleaseKeyCondition)
+
+
+
+
+		
 		self.gameLoop.drawable.append(go)
 		self.gameLoop.tickers.append(go)
 	
